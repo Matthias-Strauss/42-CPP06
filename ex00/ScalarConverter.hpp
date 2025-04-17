1b02254c21f72435b5155a0e33bf4ec9bf689c03
Matthias-Strauss <mstrauss@proton.me>
2025-04-17 18:57:16 +0200

    ex00: removing accidental .o files

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: mstrauss <mstrauss@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 16:34:54 by mstrauss          #+#    #+#             */
-/*   Updated: 2025/04/17 17:40:43 by mstrauss         ###   ########.fr       */
+/*   Updated: 2025/04/17 18:54:15 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,11 @@
 
 bool isSpecial(const std::string &str);
 void printImpossible();
+void printChar(double d);
+void printInt(double d);
+void printFloat(double d);
+void printDouble(double d);
+void handleCharInput(char c);
 
 class ScalarConverter
 {
