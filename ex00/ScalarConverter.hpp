0cda2c19d55d73b31b6972df8dbd54359ccf86ff
Matthias-Strauss <mstrauss@proton.me>
2025-04-17 16:40:28 +0200

    ex00: basic class scaffolding

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -1,24 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mstrauss <mstrauss@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/22 14:14:39 by mstrauss          #+#    #+#             */
-/*   Updated: 2025/04/17 16:36:06 by mstrauss         ###   ########.fr       */
+/*   Created: 2025/04/17 16:34:54 by mstrauss          #+#    #+#             */
+/*   Updated: 2025/04/17 16:39:11 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ScalarConverter.hpp"
+#pragma once
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+#include <iostream>
 
-int main(int argc, char **argv)
+class ScalarConverter
 {
-    if (argc != 2)
-    {
-        std::cout << "Usage: " << argv[0] << " <literal>" << std::endl;
-        return 1;
-    }
-    ScalarConverter::convert(argv[1]);
-    return 0;
-}
+
+public:
+    static void convert(const std::string &literal);
+
+private:
+    ScalarConverter() = delete;
+    ScalarConverter(const ScalarConverter &) = delete;
+    ScalarConverter &operator=(const ScalarConverter &) = delete;
+    ~ScalarConverter() = delete;
+};
+
+#endif
